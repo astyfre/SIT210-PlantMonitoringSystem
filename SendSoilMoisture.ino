bdc32742e36df4b92d4ad713dc1a8026f72d0ff2
astyfre <91183543+astyfre@users.noreply.github.com>
2022-05-22 15:57:30 +1000

    Update SendSoilMoisture.ino

--- SendSoilMoisture.ino
+++ SendSoilMoisture.ino
@@ -1,21 +1,15 @@
-const int soilPin = A4;
+const int soilPin = A1;
 
 void setup() {
+    
   Serial.begin(9600);
-
+  pinMode(soilPin,INPUT);  
 }
 
 void loop() {
   
   int sensor_analog = analogRead(soilPin);
-  float moisture_percent = (100 - ((sensor_analog/4095.00) * 100));
-  
-  String Soil_Moisture = String(soilPin);
-  Serial.print("Moisture percent");
-  Serial.print(moisture_percent);
-  Particle.publish("Plant_Moisture", Soil_Moisture);
-  delay(3600000);
-
+  float moisture_percent = (((sensor_analog/4095.00) * 100));
+  Particle.publish("Plant_Moisture", String(moisture_percent));
+  delay(3600000); //Checks once per hour
 }
-
-
